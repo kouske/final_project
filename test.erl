f324fd07aee63f357fb1051b078052c9cf59698f
Daniel Harari <danielha@mellanox.com>
2016-09-01 16:38:06 +0300

    find_cores complete with its subfunctions

--- test.erl
+++ test.erl
@@ -3,7 +3,12 @@
 
 -module(test).
 
--export([start/0]).
+-export([start/2]).
+
+start(Neighbors, MyMac) -> 
+	FragID = MyMac,
+	FragLevel = 0,
+	find_cores(lists:keySort(2, Neighbors), MyMac, FragID, FragLevel). %Neighbors -> {MAC, RSSI}.
 
 %%------------------------------------------------------%%
 %% the first part of the algorithm.
@@ -15,7 +20,29 @@
 %%
 %% edges will be identified via the MAC sddress of the connected node on the other side of the edge.
 %%------------------------------------------------------%%
-find_cores() -> .
+find_cores(Neighbors, MyMac, FragID, FragLevel) -> 
+	Best_Mac = send_core_msgs(Neighbors, MyMac),
+	Core_status = receive_core_msgs(Best_Mac),
+	if Core_status == core_found -> CORE = Best_Mac;
+	   true -> CORE = 0 end,
+	broadcast().
+
+send_core_msgs(Neighbors, MyMac) ->
+	{Best_Mac,_} = hd(Neighbors), %list is ordered, the first entry is the lowest RSSI
+	{find_core, MyMac, yes} ! Best_Mac, %send "yes" to the best path
+	[{find_core, MyMac, no} ! Dest_Mac || {Dest_Mac,_} <- Neighbors--[hd(Neighbors)]], %send "no" to all the rest.
+	Best_Mac.
+
+receive_core_msgs(Best_Mac) -> 
+	receive
+		{find_cores, Src_Mac, yes} -> if Src_Mac == Best_Mac -> core_found;
+										 true -> receive_core_msgs(Best_Mac) end;
+		{find_cores, Src_Mac, no} -> receive_core_msgs(Best_Mac);
+		_ -> erlang:display("unexpected message received, find cores")
+	after
+		5000 -> core_not_found
+	end.  
+	
 
 %%------------------------------------------------------%%
 %% initially, each node is a seperate fragment (accept for the core, the nodes connected via the core are a fragment themselves)
@@ -24,30 +51,29 @@ find_cores() -> .
 %%
 %% this stage is not needed in the first iteration of the algorithm, as every node is a fragment.
 %%------------------------------------------------------%%
-broadcast() -> .
+broadcast() -> [].
 
 %%------------------------------------------------------%%
 %% 1. leaves send information on their best path out of the fragment (the closest neighbor that is not in the frsgment). --if no neighbor is apparent, infinity value will be sent.
 %% 2. each intemediate node waits for all of its neighbors to send a message (accept for the father).
-%% 3. the node will choose the best path out of all the messages it received and its own outgiong paths and forwards it to the node from whom the prevois broadcast was received.
+%% 3. the node will choose the best path out of all the messages it received and its own outgiong paths and forwards it to the node from whom the previous broadcast was received.
 %% 4. the process will recursively continue untill it reaches the core.
 %%------------------------------------------------------%%
-convergecast() -> .
+convergecast() -> [].
 
 %%------------------------------------------------------%%
 %% 1. the two nodes connected to the core can now send the paths and decide on the best one.
 %% 2. a message is sent down through the branches to the node that is connected to the path.
 %% 3. the node will send a message through the path requesting to join the two fragments. the join method is dependent on the levels and Id's of the nodes on both sides of the path.
 %%------------------------------------------------------%%
-change_core() -> .
+change_core() -> [].
 
 %%------------------------------------------------------%%
 %% this function will find the best outgoing path.
 %%
 %% 1. send a message to all paths with frag ID and level.
 %% 2. wait for a response that will tell if the path is outgoing or not.
-%% 3. if the 
-%% *** WFT? is the message sent here or in change_core??? ***
+%% 3. 
 %%------------------------------------------------------%%
-find_min_outgoing() -> .
+find_min_outgoing() -> [].
 
