c6d7ca401e7ae01680f37f578a0c48ee13f59293
kouske <kouske@gmail.com>
2016-10-13 10:19:31 +0200

    dispatcher: add get self mac functionality + change get_neighbors (need to confirm that it's working)
    Signed-off-by: kouske <kouske@gmail.com>

--- erlang_src/src/os_dispatcher.erl
+++ erlang_src/src/os_dispatcher.erl
@@ -8,7 +8,7 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([get_neighbors/0, scan/2, add_peer/1, remove_peer/1, block_connection/0]).
+-export([get_neighbors/0, scan/2, add_peer/1, remove_peer/1, block_connection/0, get_self_mac/0]).
 
 
 
@@ -18,7 +18,8 @@
 
 get_neighbors() ->
 	Signal = [X || X <- string:tokens(os:cmd("iw dev mesh0 station dump | grep 'signal avg'"), "\n\t "), X /= "signal" , X /= "avg:", X /= "dBm"],
-	MAC = [Y || Y <- string:tokens(os:cmd("iw dev mesh0 station dump | grep Station"), "\n\t "), Y /= "Station" , Y /= "(on", Y/= "mesh0)"],
+	MAC = [list_to_atom(string:to_upper(string:join(string:tokens(Y, ":"),""))) ||
+			Y <- string:tokens(os:cmd("iw dev mesh0 station dump | grep Station"), "\n\t "), Y /= "Station" , Y /= "(on", Y/= "mesh0)"],
 	lists:zip(MAC, Signal).
 
 block_connection() ->
@@ -34,3 +35,9 @@ scan(Network_Name, Channel) ->
 	os:cmd("wpa_cli -i mesh0 scan"),
 	WPA_Results = string:tokens(os:cmd("wpa_cli -i mesh0 scan_r | grep MESH | grep " ++ Network_Name ++ " | grep " ++ Channel), "\n\t "),
 	[{X} || X <- WPA_Results, X /= "[MESH]", X /= Channel, X/= Network_Name].
+
+get_self_mac() ->
+	string:tokens(os:cmd("ifconfig | awk '$0 ~ /mesh0/ { print $5 }'"), "\n\t ").
+
+
+%% ifconfig | awk '$0 ~ /mesh0/ { print $5 }' | sed 's/://g' 
