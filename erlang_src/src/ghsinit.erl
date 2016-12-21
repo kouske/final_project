37f9bb166b2b9b286cc2d4d1f8f88561e0ee7d9d
Altshul <maxim.altshul@ti.com>
2016-12-21 16:42:07 +0200

    hardcode neighbors (for now)
    Signed-off-by: Altshul <maxim.altshul@ti.com>

--- erlang_src/src/ghsinit.erl
+++ erlang_src/src/ghsinit.erl
@@ -73,7 +73,7 @@ init() ->
 	MyMac = os_dispatcher:get_self_mac(),
 	MyIP = os_dispatcher:get_self_ip(),
 	Neighbors = os_dispatcher:get_neighbors(),
-	Neighbors_IP = os_dispatcher:get_all_nodes(),
+	Neighbors_IP = [{'7cec79c842b5','10.20.30.44'}, {'7cec79c80147', '10.20.30.43'}, {'7cec79c9dbf4','10.20.30.42'},{'689e19049082', '10.20.30.41'}],
 	
 	% start the node
 	net_kernel:start([list_to_atom(MyMac ++ "@" ++ MyIP), longnames]),
